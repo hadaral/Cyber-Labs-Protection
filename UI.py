fa48b729aa95570955e839c2e5c83727006f1fe6
hadaral <hadaral1994@gmail.com>
2018-05-03 12:40:41 +0300

    choose two dates

--- UI.py
+++ UI.py
@@ -6,7 +6,7 @@ import monitor
 import datetime
 import time
 
-choose_bet = raw_input("please enter 1 to monitor or 2 to ")
+choose_bet = raw_input("please enter 1 to monitor or 2 to see differences between two dates ")
 ch = False
 while ch == False:
     if choose_bet == "1":
@@ -14,10 +14,9 @@ while ch == False:
         ch = True
 
     elif choose_bet == "2":
-        def start_monitor():
-            observer = Files_Handler.Observer()
-            observer.schedule(Files_Handler.MyHandler(), '.')
-            observer.start()
+        observer = Files_Handler.Observer()
+        observer.schedule(Files_Handler.MyHandler(), '.')
+        observer.start()
 
         isValid = False
         while not isValid:
@@ -28,22 +27,39 @@ while ch == False:
             except:
                 print "Input doesn't match the pattern, try again!\n"
         isValid = False
-
         date1 = date1+":00"
 
-        date2 = raw_input("please enter the second date in format : yyyy-mm-dd hh:mm")  #check the format
+        while not isValid:
+            date2 = raw_input("please enter the second date in format : yyyy-mm-dd hh:mm")  #check the format
+            try:  # strptime throws an exception if the input doesn't match the pattern
+                datetime.datetime.strptime(date2, "%Y-%m-%d %H:%M")
+                isValid = True
+            except:
+                print "Input doesn't match the pattern, try again!\n"
+        #isValid = False
         date2 = date2+":00"
-        if part2.two_dates(date1,date2) == 0 :
-            print "both date doesn't exist or invalid format please try agaim\n"
-        elif part2.two_dates(date1,date2) == -1 :
-            print "the first date doesn't exist or invalid format please try again\n"
-        elif part2.two_dates(date1,date2) == -2 :
-            print "the second date doesn't exist or invalid format please try again\n"
+        if date1 > date2 :
+            ch = False
+            print "last date earlyer then the first. try again."
+            continue
+        if date1 == date2:
+            ch = False
+            print "same date. try again."
+            continue
+        list_one = []
+        list_two = []
+        if part2.two_dates(list_one,list_two,date1,date2) == -1:
+            print "first date doesn'n exist please try again."
+        elif part2.two_dates(list_one,list_two,date1,date2) == -2:
+            print "last date doesn'n exist please try again."
+        elif part2.two_dates(list_one,list_two,date1,date2) == 0:
+            print "both dates doesn'n exist please try again."
         else:
-
+            part2.printing(list_one,list_two)
             ch = True
+
     else:
-        choose_bet = raw_input("*wrong choose*. please enter 1 to monitor or 2 to ")
+        choose_bet = raw_input("*wrong choose*. please enter 1 to monitor or 2 to see differences between two dates ")
 
 
 
--- part2.py
+++ part2.py
@@ -1,19 +1,16 @@
 
 
-def two_dates(date_first, date_last):
+def two_dates(list_one, list_two, date1, date2):
 
     file_ = open("processList.txt","r")
     file = file_.readlines()
     file_.close()
-    date1 = date_first
-    date2 = date_last
-    list_one = []
-    list_two = []
+    #list_one = []
+    #list_two = []
     for i in range(len(file)):
         if file[i] != "\n":
             file[i].replace("\n","")
             string = file[i].split(",")
-
             date = string[1].replace("\n","")
             date.replace("\n","")
             if str(date)==str(date1) and string[0]not in list_one:
@@ -23,28 +20,17 @@ def two_dates(date_first, date_last):
     if not list_one and not list_two:
         return 0
     elif not list_one and list_two:
-        for k in range(len(list_two)):
-            print list_two[k] , " at " , date_last
-        return -1
+            return -1
     elif not list_two and list_one:
-        for l in range(len(list_one)):
-            print list_one[l] , " at " , date_first , "  *****" , l
         return -2
-    else:
+
+
+def printing(list_one, list_two):
         for l in range(len(list_one)):
             if list_one[l] not in list_two:
-                print list_one[l] , " at " , date_first , "  *****" , l
+                print list_one[l] , " got finished "
+
 
         for k in range(len(list_two)):
             if list_two[k] not in list_one:
-                print list_two[k] , " at " , date_last
-
-
-def printing(list_one, list_two,date_first, date_last):
-        for l in range(len(list_one)):
-            if list_one[l] not in list_two:
-                print list_one[l] , " at " , date_first , "  *****" , l
-
-        for k in range(len(list_two)):
-            if list_two[k] not in list_one:
-                print list_two[k] , " at " , date_last
+                print list_two[k] , " got created "
